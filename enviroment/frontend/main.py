434e3a0d2aca00a3638a9c381d65e8ce9df3970a
sungho <test@encore.com>
2025-03-18 11:47:36 +0900

    page1 수정

--- enviroment/frontend/main.py
+++ enviroment/frontend/main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from common.page1 import app as page1_app
+from interview.page1 import app as page1_app
 from common.page2 import app as page2_app
 from common.create_q import app as create_q_app
 from common.image_ocr import app as ocr_app
@@ -18,10 +18,15 @@ user_info = requests.get(os.environ.get('API_URL') + "/get_user_info")
 st.sidebar.title(test_req.json()['Hello'])
 page = st.sidebar.radio("Go to", ("Page 1", "Page 2", user_info.json()['user_info'][1] + "님 안녕하세요", "질문 생성", "이미지 OCR"))
 
+st.sidebar.button("면접관리")
+st.sidebar.button("추천공고")
+st.sidebar.button("모의면접")
+st.sidebar.button("면접 히스토리")
+
 if page == "Page 1":
     page1_app()
 elif page == "Page 2":
-    page2_app()
+    page2_app()  
 elif page == "질문 생성":
     create_q_app()
 elif page == "이미지 OCR":
