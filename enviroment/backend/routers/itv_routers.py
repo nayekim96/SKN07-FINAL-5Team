24f918c88852321ea5de782abde86163dc900bf3
roky <roky@got.com>
2025-04-08 10:31:05 +0900

    2025.04.08 pmh 기업/직무 선택, 장비테스트, 면접시작 페이지 개발

--- enviroment/backend/routers/itv_routers.py
+++ enviroment/backend/routers/itv_routers.py
@@ -1,4 +1,32 @@
 # 모의면접 Routers
 from fastapi import APIRouter
+from db_util.db_utils import post_db_connect
 
 router = APIRouter(prefix="/itv")
+common_select_text = {"선택해주세요":"None"}
+
+@router.get('/get_company_list', status_code=200)
+def get_company_list():
+    connect = post_db_connect()
+    result = connect.select_all("""select ccmt.common_id as common_id, 
+                                          ccmt.common_nm as common_nm
+                                   from company_code_master_tbl ccmt;""")
+    connect.close()
+    return common_select_process(result, 'common_nm', 'common_id', 'company_list')
+
+
+@router.get('/get_job_list', status_code=200)
+def get_company_list():
+    connect = post_db_connect()
+    result = connect.select_all("""select jcmt.common_id as common_id,
+                                          jcmt.common_nm as common_nm
+                                   from job_code_master_tbl jcmt;""")
+    connect.close()
+    return common_select_process(result, 'common_nm', 'common_id', 'job_list')
+
+def common_select_process(select_list:dict, key:str, value: str, list_nm: str):
+    select_box = common_select_text.copy()
+    sum_dict = {i[key] : i[value] for i in select_list}
+    select_box.update(sum_dict)
+    del sum_dict
+    return {list_nm: select_box, 'labels': list(select_box.keys())}
--- enviroment/frontend/main.py
+++ enviroment/frontend/main.py
@@ -1,15 +1,4 @@
 import streamlit as st
-from interview.mng1 import app as page1_app  # 이력서 업로드 페이지
-from common.rec1 import app as page2_app  # 추천공고 랜덤 
-from common.rec2 import app as page3_app  # 추천공고 맞춤춤
-from interview.itv1 import app as ocr1_app  # 모의면접 - 옵션선택
-from interview.itv2 import app as ocr2_app  # 모의면접 - 장비 테스트
-from interview.itv3 import app as ocr3_app  # 모의면접 - 시작화면
-from interview.itv4 import app as ocr4_app  # 모의면접 - 시작 타이머
-from interview.itv5 import app as ocr5_app  # 모의면접 - 실전 면접
-from history.his1 import app as his1_app  # 히스토리
-from history.his2 import app as his2_app  # 히스토리 - 종합 레포트
-from history.his3 import app as his3_app  # 히스토리 - 상세 피드백
 import requests
 from dotenv import load_dotenv
 import os
@@ -50,46 +39,5 @@ except requests.exceptions.RequestException as e:
     test_req, user_info = None, None
     st.error(f"백엔드 연결 실패: {e}")
 
-
-# 사이드바 메뉴 추가
-st.sidebar.title("메뉴")
-if st.sidebar.button("면접관리"):
-    st.session_state.page = "mng_1"
-    st.rerun()
-if st.sidebar.button("추천공고"):
-    st.session_state.page = "rec_1"
-    st.rerun()
-if st.sidebar.button("모의면접"):
-    st.session_state.page = "itv1"
-    st.rerun()
-if st.sidebar.button("면접 히스토리"):
-    st.session_state.page = "his1"
-    st.rerun()
-
-# 페이지 이동 로직
-if st.session_state.page == "mng_1":
-    page1_app()
-elif st.session_state.page == "rec_1":
-    page2_app()
-elif st.session_state.page == "rec_2":
-    page3_app()
-elif st.session_state.page == "itv1":
-    ocr1_app()
-elif st.session_state.page == "itv2":
-    ocr2_app()
-elif st.session_state.page == "itv3":
-    ocr3_app()
-elif st.session_state.page == "itv4":
-   ocr4_app()
-elif st.session_state.page == "itv5":
-   ocr5_app()
-elif st.session_state.page == "his1":
-    his1_app()
-elif st.session_state.page == "his2":
-    his2_app()
-elif st.session_state.page == "his3":
-    his3_app()
-else:
-    st.write("잘못된 페이지입니다.")
 
 st.switch_page("pages/main_page.py")
