c22d8770ffd68a2009b8098fc13f710bb34aeae8
nayekim <nayegraphy@gmail.com>
2025-04-09 10:24:20 +0900

    db_utils 내 select_all 함수 누락 건 수정

--- enviroment/backend/db_util/db_utils.py
+++ enviroment/backend/db_util/db_utils.py
@@ -24,6 +24,10 @@ class post_db_connect():
         self.cursor.execute(query)
         return self.cursor.fetchmany(num)
 
+    def select_all(self, query: str):
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+    
     def select_many_vars(self, query:str, conditions, num: int):
         self.cursor.execute(query, conditions)
         return self.cursor.fetchmany(num)
@@ -31,6 +35,11 @@ class post_db_connect():
     def insert_many_vars(self, query: str, conditions):
         self.cursor.execute(query, conditions)
         return self.db.commit()
+    
+    def excute_crud(self, query: str):
+        result = self.cursor.execute(query)
+        self.db.commit()
+        return result
     
     def close(self):
         self.cursor.close()
