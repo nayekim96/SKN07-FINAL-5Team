a18622fe7b459a96735302c4ab6f3275a987ce07
nayekim <nayegraphy@gmail.com>
2025-04-23 19:17:15 +0900

    resolve conflict

--- enviroment/frontend/pages/equipment_test.py
+++ enviroment/frontend/pages/equipment_test.py
@@ -2,112 +2,11 @@ import streamlit as st
 from sidebar import show_sidebar
 import time
 import cv2
-
-st.set_page_config(layout="wide")
-show_sidebar()
-# 페이지 상단 공백 제거 markdown
-st.markdown(
-    """
-        <style>
-                .stAppHeader {
-                    background-color: rgba(255, 255, 255, 0.0);  /* Transparent background */
-                    visibility: visible;  /* Ensure the header is visible */
-                }
-
-            .block-container {
-                    padding-top: 1rem;
-                    padding-bottom: 0rem;
-                    padding-left: 5rem;
-                    padding-right: 5rem;
-                }
-                [data-testid="stSidebarNav"] {display: none;}
-        </style>
-        """,
-    unsafe_allow_html=True,
-)
-
-st.title("장비 테스트")
-
-enable = st.checkbox("카메라 체크")
-#picture = st.camera_input("Take a picture", disabled=not enable)
-
-webcam = cv2.VideoCapture(0, cv2.CAP_V4L2)
-
-if not webcam.isOpened():
-    print("Could not open .webcam")
-    exit()
-
-
-
-frame_window = None
-
-if enable:
-
-    while enable:
-        frame_window = st.image([])
-
-        #cap = cv2.VideoCapture(0)
-        webcam.set(cv2.CAP_PROP_FPS, 10)  # FPS 속성 설정 (지원하는 경우)
-
-        frame_count = 0
-        while webcam.isOpened():
-            ret, frame = webcam.read()
-            if not ret:
-                st.error("카메라에서 영상을 읽어올 수 없습니다.")
-                break
-
-            frame_count += 1
-            # 3프레임 중 1프레임만 처리하여 부하 줄이기
-            if frame_count % 3 != 0:
-                continue
-
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            frame_window.image(frame)
-            time.sleep(0.1)  # 루프 지연 추가
-
-        st.write("스트리밍 중지")
-        webcam.release()
-
-
-        # while webcam.isOpened():
-        #     status, frame = webcam.read()
-
-        #     if status:
-        #         cv2.imshow("test", frame)
-        #     if cv2.waitKey(1) & 0xFF == ord('q'):
-        #         break
-
-
-        # webcam.release()
-        # cv2.destroyAllWindows()
-elif frame_window != None and not enable:
-    # webcam.release()
-    # cv2.destroyAllWindows()
-    frame_window.empty()
-
-# if picture:
-#     st.image(picture)
-
-
-audio_value = st.audio_input("마이크 체크")
-if audio_value:
-    st.audio(audio_value)
-
-
-if st.checkbox("2가지 모두 정상"):
-    time.sleep(1)
-    st.switch_page("pages/itv1.py")
-
-
-
-
-
-    
-=======
 import tempfile
 import openai
 import os
 from dotenv import load_dotenv
+
 
 load_dotenv()
 # OpenAI API 키 설정
@@ -157,7 +56,12 @@ def main():
     #st.write('1. 웹캠 테스트 :no_entry_sign:')
     
     webcam = cv2.VideoCapture(0, cv2.CAP_V4L2)
+
     st.write(st.session_state.webcam_bool)
+
+    # ##### 임시!!!! 웹캠 테스트 강제 완료 설정
+    st.session_state.webcam_bool = True
+
     if not webcam.isOpened() and st.session_state.webcam_bool == False:
         webcam_placeholder.write('1. 웹캠 테스트 :no_entry_sign:')
     else:
@@ -169,7 +73,7 @@ def main():
     # # 컨테이너 생성
     # container = st.container()
 
-    
+    st.write('테스트')
     
     if st.session_state.test_count == 0:
         # 컨테이너 내부 UI 요소를 비우기 위한 empty() 객체
@@ -271,13 +175,6 @@ def speak_test():
             speaker_placeholder.write("3. 스피커 테스트 :white_check_mark:")
         st.session_state.speaker_bool = True
 
-
-
-def test_done():
-    if st.button('테스트 완료'):
-        time.sleep(1)
-        st.switch_page("pages/mng_2.py")
-
 if __name__ == "__main__":
     #st.switch_page("pages/mng_2.py")
     main()
@@ -290,6 +187,7 @@ if __name__ == "__main__":
         with col1:
             st.write("테스트가 완료되었으면 준비완료 버튼을 클릭하세요")
         with col2:
-            if st.button('테스트 완료', key="test_done"):
+            if st.button('테스트 완료'):
                 time.sleep(1)
-                st.switch_page("pages/itv1.py")
+
+                st.switch_page("pages/itv.py")
