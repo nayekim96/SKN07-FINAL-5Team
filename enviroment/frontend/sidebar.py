2b3b38e4ba70ff4a678efb544c41a36a20804b29
jjaecheol <wowgom10@gmail.com>
2025-04-28 09:28:18 +0900

    Update sidebar.py

--- enviroment/frontend/sidebar.py
+++ enviroment/frontend/sidebar.py
@@ -6,9 +6,6 @@ def show_sidebar():
     if st.sidebar.button("메인페이지", key="main_page_button"):
         st.switch_page("pages/main_page.py")
 
-    if st.sidebar.button("면접관리", key="mng_1_button"):
-        st.switch_page("pages/mng_1.py")
-
     if st.sidebar.button("서류 제출", key="upload_history_button"):
         st.switch_page("pages/user_pdf_upload.py")
 
@@ -16,7 +13,7 @@ def show_sidebar():
         st.switch_page("pages/recruit.py") 
 
     if st.sidebar.button("모의면접", key="mock_interview_button"):
-        st.switch_page("pages/mng_1.py")
+        st.switch_page("pages/itv.py")
 
     if st.sidebar.button("면접 히스토리", key="history_button"):
         st.switch_page("pages/his1.py")
